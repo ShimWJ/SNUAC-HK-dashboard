88afd7abc3745c15fa2bf9ece9ebc91856168120
ShimWJ <162956027+ShimWJ@users.noreply.github.com>
2024-03-14 15:28:02 +0900

    Add files via upload

--- 0_st_test.py
+++ 0_st_test.py
@@ -37,8 +37,8 @@ with st.sidebar:
                      '90-94 age population', '95-99 age population', '100+ age population']
     selected_variable = st.selectbox('Select a variable', variable_list)
     
-    df_selected_table = df_reshaped.loc[df_reshaped.Year == selected_year, ['region', 'country', 'Year', selected_variable]]
-    df_selected_table = df_selected_table.style.hide_index()
+    df_selected_table = df_reshaped.loc[df_reshaped.Year == selected_year, ['region', 'country', 'ISO3', 'Year', selected_variable]]
+    #df_selected_table = df_selected_table.style.hide_index()
 
 st.header(str(selected_year)+' '+str(selected_variable)+' '+ 'in Asia')
 st.dataframe(df_selected_table)
@@ -51,30 +51,34 @@ st.dataframe(df_selected_table)
 #######################
 # Plots
 
-# Heatmap
-def make_heatmap(input_df, input_y, input_x, input_color, input_color_theme):
-    heatmap = alt.Chart(input_df).mark_rect().encode(
-            y=alt.Y(f'{input_y}:O', axis=alt.Axis(title="Year", titleFontSize=18, titlePadding=15, titleFontWeight=900, labelAngle=0)),
-            x=alt.X(f'{input_x}:O', axis=alt.Axis(title="", titleFontSize=18, titlePadding=15, titleFontWeight=900)),
-            color=alt.Color(f'max({input_color}):Q',
-                             legend=None,
-                             scale=alt.Scale(scheme=input_color_theme)),
-            stroke=alt.value('black'),
-            strokeWidth=alt.value(0.25),
-        ).properties(width=900
-        ).configure_axis(
-        labelFontSize=12,
-        titleFontSize=12
-        ) 
-    # height=300
-    return heatmap
-
 # Choropleth map
+#df_selected_table_for_map = df_selected_table
+#df_selected_table_for_map.rename(columns = {str(selected_variable):'selected_variable'}, inplace=True)
+
+# def make_choropleth(input_df, input_id, input_column, input_color_theme):
+#     choropleth = px.choropleth(input_df, locations=input_id, color=input_column,
+#                                color_continuous_scale=input_color_theme,
+#                                range_color=(0, max(df_selected_table_for_map.selected_variable)),
+#                                scope="asia",
+#                                labels={'population':'Population'}
+#                               )
+#     choropleth.update_layout(
+#         template='plotly_dark',
+#         plot_bgcolor='rgba(0, 0, 0, 0)',
+#         paper_bgcolor='rgba(0, 0, 0, 0)',
+#         margin=dict(l=0, r=0, t=0, b=0),
+#         height=350
+#     )
+#     return choropleth
+
+# choropleth = make_choropleth(df_selected_table_for_map, 'ISO3', 'selected_variable', 'blues')
+# st.plotly_chart(choropleth)
+
 def make_choropleth(input_df, input_id, input_column, input_color_theme):
-    choropleth = px.choropleth(input_df, locations=input_id, color=input_column, locationmode="USA-states",
+    choropleth = px.choropleth(input_df, locations=input_id, color=input_column,
                                color_continuous_scale=input_color_theme,
-                               range_color=(0, max(df_selected_year.population)),
-                               scope="usa",
+                               range_color=(min(df_selected_table[selected_variable]), max(df_selected_table[selected_variable])),
+                               scope="asia",
                                labels={'population':'Population'}
                               )
     choropleth.update_layout(
@@ -85,3 +89,6 @@ def make_choropleth(input_df, input_id, input_column, input_color_theme):
         height=350
     )
     return choropleth
+
+choropleth = make_choropleth(df_selected_table, 'ISO3', df_selected_table[selected_variable], 'blues')
+st.plotly_chart(choropleth)
