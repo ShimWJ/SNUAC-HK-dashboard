c76d4fd4d838c81f71dfa3330d96c1e31e333693
ShimWJ <162956027+ShimWJ@users.noreply.github.com>
2024-07-02 16:55:25 +0900

    Update interactive_map_dashboard.py

--- interactive_map_dashboard.py
+++ interactive_map_dashboard.py
@@ -36,13 +36,14 @@ with st.sidebar:
     organization_list = ['UN', 'SNUAC', '외교부', 'World Bank', 'EU']
     selected_organization = st.selectbox('Select a organization', organization_list)
     #variable_list = 
-    #df_selected_table = df_region_criteria.loc
+    df_selected_table = df_region_criteria.loc[:,['country','ISO3',selected_organization]]
 
     
     #df_selected_table = df_reshaped.loc[df_reshaped.Year == selected_year, ['region', 'country', 'ISO3', 'Year', selected_variable]]
     #df_selected_table = df_selected_table.style.hide_index()
 
-st.header(str(selected_year)+' '+str(selected_variable)+' '+ 'in Asia')
+st.header(str(selected_organization)+' '+ 'in Asia')
+#st.header(str(selected_year)+' '+str(selected_variable)+' '+ 'in Asia')
 st.dataframe(df_selected_table)
 
 
